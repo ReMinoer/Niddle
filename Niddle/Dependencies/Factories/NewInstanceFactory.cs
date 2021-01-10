a18cd1bc55885a6f402066ab9a044767ee8b4ada
ReMinoer <reminoer@gmail.com>
2021-01-10 13:30:06 +0100

    Fix NewInstanceFactory resolvable members

--- Niddle/Dependencies/Factories/NewInstanceFactory.cs
+++ Niddle/Dependencies/Factories/NewInstanceFactory.cs
@@ -12,7 +12,8 @@ namespace Niddle.Dependencies.Factories
         static private readonly Stack<IDependencyFactory> FactoryStack = new Stack<IDependencyFactory>();
         private bool _alreadyInvoke;
         private readonly IResolvableRejecter<object, IEnumerable, IEnumerable, object> _resolvableInstantiator;
-        private readonly IResolvableInjectable<object, object, object>[] _resolvableMembers;
+        private readonly IResolvableMembersProvider<object> _resolvableMembersProvider;
+
         public override InstanceOrigin? InstanceOrigin => Niddle.InstanceOrigin.Instantiation;
 
         public NewInstanceFactory(Type type, object serviceKey, IResolvableRejecter<object, IEnumerable, IEnumerable, object> resolvableInstantiator, Substitution substitution,
@@ -20,7 +21,7 @@ namespace Niddle.Dependencies.Factories
             : base(type, serviceKey, substitution)
         {
             _resolvableInstantiator = resolvableInstantiator;
-            _resolvableMembers = resolvableMembersProvider?.ForType(type).ToArray();
+            _resolvableMembersProvider = resolvableMembersProvider;
         }
 
         public override object Get(IDependencyResolver resolver)
@@ -33,8 +34,9 @@ namespace Niddle.Dependencies.Factories
 
             object instance = _resolvableInstantiator.ResolveAndReject(resolver, null);
 
-            if (_resolvableMembers != null)
-                foreach (IResolvableInjectable<object, object, object> resolvableMember in _resolvableMembers)
+            IResolvableInjectable<object, object, object>[] resolvableInjectables = _resolvableMembersProvider?.ForType(instance.GetType()).ToArray();
+            if (resolvableInjectables != null)
+                foreach (IResolvableInjectable<object, object, object> resolvableMember in resolvableInjectables)
                     resolvableMember.TryResolveAndInject(resolver, instance);
 
             _alreadyInvoke = false;
